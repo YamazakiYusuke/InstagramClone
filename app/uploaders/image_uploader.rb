e816d40bfcaac812cac379fa9111397ce1d9a43c
yamazakiyusuke <yusuke38098@gmail.com>
2020-10-21 05:05:00 +0000

    init

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,6 +1,6 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
-  process :resize_to_limit => [200, 200] 
+  process :resize_to_limit => [300, 300] 
   storage :file
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
